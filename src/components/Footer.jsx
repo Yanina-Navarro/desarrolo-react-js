7afbd6df91999c6d6f1ee5f18fd8b9d5b693758d
Yanina Navarro <yaninatamaranavarro@gmail.com>
2026-02-03 23:54:01 -0300

    creacion del proyecto en react, instalando dependencias de firebase y vinculando credenciales, se crearon las carpetas necesarias para el desarrollo del proyecto

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Derechos reservados. Diseñado por Yanina Navarro - &copy;2026</p>
+        </footer>
+    )
+}
+
+export { Footer }
